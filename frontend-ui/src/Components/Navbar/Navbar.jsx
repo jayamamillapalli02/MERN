e9e30f58be098144c5bbb7a587abca43462ab723
jmamillapalli1s <jmamillapalli1s@semo.edu>
2024-07-09 02:05:45 -0500

    first commit

--- frontend-ui/src/Components/Navbar/Navbar.jsx
+++ frontend-ui/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import './Navbar.css' 
-import logo from
+import logo from '../Assets/Assets/logo.png'
+import cart_icon from '../Assets/Assets/cart_icon.png'
 
-function App(){
+const Navbar = () =>{
+
     return (<div className = 'navbar'>
+             <div className ='nav-logo'>
+               <img src={logo} alt='' />
+               <p>SHOPPINGCART</p> 
+             </div>
+             <ul className='nav-menu'>
+                <li>Shop<hr/></li>
+                <li>Men</li>
+                <li>Women</li>
+                <li>kids</li>
+             </ul>
+             <div className = 'nav-login-cart'>
+                <button>Login</button>
+                <img src ={cart_icon} alt='' />
+                <div className = 'nav-cart_count'>0</div>
 
-    </div>)
-};
+             </div>
+
+    </div>
+    );
+}
 
 export default Navbar;
